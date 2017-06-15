41dec9127a0a10878382e3d7644ab8afd715763c
Ryan Mills <ranemills@gmail.com>
2017-06-15 21:45:15 +0100

    Flesh out basic functionality

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,10 +1,54 @@
 angular.module('methodPrompterApp', [])
-.controller('mainCtrl', function () {
+.constant('_', window._)
+.controller('mainCtrl', function (combinationService) {
   let mainCtrl = this;
 
   mainCtrl.$onInit = function() {
+    mainCtrl.newMethod = '';
+    mainCtrl.stage = 0;
 
+    mainCtrl.stages = {
+      'Doubles': 5,
+      'Minor': 6,
+      'Triples': 7,
+      'Major': 8,
+    };
   };
 
+  // Always one fewer place bells than number of bells ringing the method
+  mainCtrl.addMethod = function() {
+    combinationService.addNewMethod(mainCtrl.newMethod, mainCtrl.stage - 1)
+  };
+
+  mainCtrl.getMethods = function() {
+    return combinationService.getMethods();
+  }
+
 })
-.constant('_', window._);
+.service('combinationService', function() {
+
+  let methods = [];
+
+  // We assume that 1 is not a place bell that anyone wants to learn at the moment
+  function addNewMethod(method, numPlaceBells)
+  {
+    methods.push({
+      name: method,
+      numPlaceBells
+    });
+  }
+
+  function getMethods() {
+    return methods;
+  }
+
+  function getAllPossibilities() {
+
+  }
+
+  return {
+    addNewMethod,
+    getMethods
+  }
+
+});
