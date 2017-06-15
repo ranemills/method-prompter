c92def2808f0a10fa18e52ca1e54d041e8e9b131
Ryan Mills <ranemills@gmail.com>
2017-06-15 21:07:28 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,75 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  require('load-grunt-tasks')(grunt);
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.initConfig({
+    concat: {
+      js: {
+        src: [
+          'bower_components/jquery/dist/jquery.js',
+          'bower_components/tether/dist/js/tether.min.js',
+          'bower_components/bootstrap/dist/js/bootstrap.min.js',
+          'bower_components/angular/angular.js',
+          'bower_components/lodash/dist/lodash.js',
+          'bower_components/moment/min/moment.min.js',
+          'bower_components/angular-moment/angular-moment.min.js',
+          'bower_components/angular-timer/dist/angular-timer.js',
+          'bower_components/humanize-duration/humanize-duration.js',
+          'bower_components/chart.js/dist/Chart.js',
+          'bower_components/angular-chart.js/dist/angular-chart.min.js'
+        ],
+        dest: 'dist/third-party.js'
+      },
+      css: {
+        src: [
+          'bower_components/bootstrap/dist/css/bootstrap.css'
+        ],
+        dest: 'dist/third-party.css'
+      }
+    },
+    babel: {
+      options: {
+        sourceMap: true,
+        presets: ['es2015']
+      },
+      dist: {
+        files: {
+          'dist/app.js': 'src/scripts/app.js'
+        }
+      }
+    },
+    copy: {
+      html: {
+        expand: true,
+        cwd: 'src/html',
+        src: '**',
+        dest: 'dist/'
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['src/scripts/app.js'],
+        tasks: ['babel'],
+        options: {
+          livereload: true
+        }
+      },
+      html: {
+        files: ['src/html/**/*.html', 'src/html/index.html'],
+        tasks: ['copy:html'],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['babel', 'concat:js', 'concat:css', 'copy:html']);
+  grunt.registerTask('serve', ['default', 'watch']);
+
+};
---
+++ src/scripts/app.js
@@ -0,0 +1,10 @@
+angular.module('methodPrompterApp', [])
+.controller('mainCtrl', function () {
+  let mainCtrl = this;
+
+  mainCtrl.$onInit = function() {
+
+  };
+
+})
+.constant('_', window._);
